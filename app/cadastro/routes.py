c207b751c47ff91d099f3d9854be6ce15894e79c
Jader Campos <jaderkayque@yahoo.com.br>
2025-08-14 11:19:32 -0300

    atualizei o forms de cadastro para um teste 3

--- app/cadastro/routes.py
+++ app/cadastro/routes.py
@@ -7,6 +7,6 @@ cadastro_bp = Blueprint("cadastro", __name__, template_folder="templates")
 def cadastro():
     form = DocumentoForm()
     if request.method == 'POST':
-        print('teste')
+        print('teste 3')
 
     return render_template('cadastro/cadastrar.html', form=form)
