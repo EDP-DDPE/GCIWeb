041f5023799b9d8f26bcb02c857f59d3daddede5
Jader Campos <jaderkayque@yahoo.com.br>
2025-08-27 14:52:42 -0300

    Adicionado msg de boas vindas na tela inicial

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,9 +1,18 @@
 from flask import Blueprint, render_template, session, redirect, url_for, jsonify
 import requests
 from app.database import db_manager
+import datetime
+import json
 
 main_bp = Blueprint("main", __name__, template_folder='templates', static_folder='static')
 
+def msg_boas_vidas(nome):
+    if 12 > datetime.datetime.now().hour > 4:
+        return f"Bom dia, {nome}."
+    elif 18 > datetime.datetime.now().hour > 12:
+        return f"Boa tarde, {nome}."
+    else:
+        return f"Bom noite, {nome}."
 
 @main_bp.route("/")
 def home():
@@ -16,8 +25,14 @@ def home():
     #         <a href="/me">Chamar Microsoft Graph /me</a> |
     #         <a href="auth/logout">Sair</a>
     #     """
-
-    return render_template("main/index.html", usuario=usuario)
+    token = session.get("access_token")
+    resp = requests.get(
+        "https://graph.microsoft.com/v1.0/me",
+        headers={"Authorization": f"Bearer {token}"},
+        timeout=15,
+    )
+    nome = json.loads(resp.text)['givenName']
+    return render_template("main/index.html", usuario=usuario, msg=msg_boas_vidas(nome))
 
 
 @main_bp.route('/health')
