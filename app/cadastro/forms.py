3822bd65c90b49e275041fbb2cffcbb84cc87947
Jader Campos <jaderkayque@yahoo.com.br>
2025-08-27 13:28:16 -0300

    Alteração do Forms de cadastro

--- app/cadastro/forms.py
+++ app/cadastro/forms.py
@@ -4,47 +4,12 @@ from wtforms import StringField, FloatField, SelectField, DateField, TextAreaFie
 from wtforms.validators import DataRequired, Optional, NumberRange
 from datetime import date
 
-
 ALLOWED_EXTENSIONS = {'pdf', 'doc', 'docx', 'xls', 'xlsx', 'jpg', 'jpeg', 'png', 'dwg', 'kmz', 'kml'}
-
-
-class DocumentoForm(FlaskForm):
-    protocolo = StringField('Protocolo', validators=[DataRequired()])
-    num_doc = StringField('Número do Documento', validators=[DataRequired()])
-    nome_cliente = StringField('Nome do Cliente', validators=[DataRequired()])
-    cnpj = StringField('CNPJ', validators=[DataRequired()])
-    instalacao = StringField('Instalação', validators=[DataRequired()])
-    pass
-    tipo_viab = SelectField('Tipo de Viabilidade', coerce=int, validators=[DataRequired()])
-    tipo_analise = SelectField('Tipo de Análise', coerce=int, validators=[DataRequired()])
-    tipo_pedido = SelectField('Tipo de Pedido', coerce=int, validators=[DataRequired()])
-
-    dem_fp_ant = FloatField('Demanda FP Anterior', validators=[Optional()])
-    dem_p_ant = FloatField('Demanda P Anterior', validators=[Optional()])
-    dem_fp_dep = FloatField('Demanda FP Depois', validators=[Optional()])
-    dem_p_dep = FloatField('Demanda P Depois', validators=[Optional()])
-
-    municipio = SelectField("Município", coerce=int, validators=[DataRequired()])
-    latitude_x = FloatField('Latitude X', validators=[Optional()])
-    longitude_y = FloatField('Longitude Y', validators=[Optional()])
-
-    area_resp = SelectField('Área Responsável',
-                            choices=[(0, 'Selecione...'), ('norte', 'Norte'), ('at', 'AT'), ('projeto', 'Projeto'),
-                                     ('guarulhos', 'Guarulhos')])
-    elaborador_doc = StringField('Elaborador do Documento', validators=[DataRequired()])
-    eng_responsavel = StringField('Engenheiro Responsável', validators=[DataRequired()])
-
-    arquivo = FileField('Anexar Documento',
-                        validators=[FileRequired(), FileAllowed(ALLOWED_EXTENSIONS, 'Tipo de arquivo não permitido!')])
-
-    # SelectFields para relacionamentos (você vai popular com query do banco)
-    empresa = SelectField("Empresa", coerce=int, validators=[DataRequired()])
-    regional = SelectField("Regional", coerce=int, validators=[DataRequired()])
 
 
 class EstudoForm(FlaskForm):
     # Campos básicos do estudo
-    num_doc = StringField('Número do Documento', validators=[DataRequired()])
+    num_doc = StringField('Número do Documento', validators=[DataRequired()], render_kw={"readonly": True})
     protocolo = StringField('Protocolo', validators=[Optional()])
     nome_projeto = TextAreaField('Nome do Projeto', validators=[DataRequired()])
     descricao = TextAreaField('Descrição', validators=[Optional()])
@@ -58,10 +23,22 @@ class EstudoForm(FlaskForm):
                                   default=0)
 
     # Demandas solicitadas
-    dem_solicit_fp = FloatField('Demanda Solicitada FP (kW)',
-                                validators=[DataRequired(), NumberRange(min=0)])
-    dem_solicit_p = FloatField('Demanda Solicitada P (kW)',
-                               validators=[DataRequired(), NumberRange(min=0)])
+    dem_carga_atual_fp = FloatField('Demanda Carga atual FP (kW)',
+                                    validators=[DataRequired(), NumberRange(min=0)])
+    dem_carga_atual_p = FloatField('Demanda Carga atual P (kW)',
+                                   validators=[DataRequired(), NumberRange(min=0)])
+    dem_carga_solicit_fp = FloatField('Demanda Carga Solicitada FP (kW)',
+                                      validators=[DataRequired(), NumberRange(min=0)])
+    dem_carga_solicit_p = FloatField('Demanda Carga Solicitada P (kW)',
+                                     validators=[DataRequired(), NumberRange(min=0)])
+    dem_ger_atual_fp = FloatField('Demanda Geração atual FP (kW)',
+                                  validators=[DataRequired(), NumberRange(min=0)])
+    dem_ger_atual_p = FloatField('Demanda Geração atual P (kW)',
+                                 validators=[DataRequired(), NumberRange(min=0)])
+    dem_ger_solicit_fp = FloatField('Demanda Geração Solicitada FP (kW)',
+                                    validators=[DataRequired(), NumberRange(min=0)])
+    dem_ger_solicit_p = FloatField('Demanda Geração Solicitada P (kW)',
+                                   validators=[DataRequired(), NumberRange(min=0)])
 
     # Coordenadas do cliente
     latitude_cliente = FloatField('Latitude do Cliente',
@@ -73,11 +50,18 @@ class EstudoForm(FlaskForm):
     observacao = TextAreaField('Observações', validators=[Optional()])
 
     # Datas
+
     data_registro = DateField('Data de Registro',
                               validators=[DataRequired()],
                               default=date.today)
-    data_transgressao = DateField('Data de Transgressão', validators=[Optional()])
-    data_vencimento = DateField('Data de Vencimento', validators=[Optional()])
+    data_abertura_cliente = DateField('Data de Abertura do Cliente', validators=[Optional()])
+    data_desejada_cliente = DateField('Data desejada do Cliente', validators=[Optional()])
+    data_vencimento_cliente = DateField('Data de Vencimento Cliente', validators=[Optional()])
+    data_prevista_conexao = DateField('Data prevista de Conexão', validators=[Optional()])
+    data_vencimento_ddpe = DateField('Data de Vencimento DDPE', validators=[Optional()])
+
+    # data_transgressao = DateField('Data de Transgressão', validators=[Optional()])
+    # data_vencimento = DateField('Data de Vencimento', validators=[Optional()])
 
     # SelectFields para relacionamentos
     edp = SelectField('EDP', coerce=int, validators=[DataRequired()])
@@ -87,9 +71,9 @@ class EstudoForm(FlaskForm):
     resp_regiao = SelectField('Responsável da Região', coerce=int, validators=[DataRequired()])
 
     # Tipos
-    tipo_viab = SelectField('Tipo de Viabilidade', coerce=int, validators=[DataRequired()])
-    tipo_analise = SelectField('Tipo de Análise', coerce=int, validators=[DataRequired()])
-    tipo_pedido = SelectField('Tipo de Pedido', coerce=int, validators=[DataRequired()])
+    tipo_viab = SelectField('Tipo de Viabilidade', choices=[], coerce=str, validators=[DataRequired()])
+    tipo_analise = SelectField('Tipo de Análise', choices=[], coerce=str, validators=[DataRequired()])
+    tipo_pedido = SelectField('Tipo de Pedido', choices=[], coerce=str, validators=[DataRequired()])
 
     # Arquivo anexo (opcional no cadastro inicial)
     arquivo = FileField('Anexar Documento',
@@ -106,8 +90,6 @@ class EstudoForm(FlaskForm):
         self.tipo_viab.choices = [(0, 'Selecione um tipo...')]
         self.tipo_analise.choices = [(0, 'Selecione um tipo...')]
         self.tipo_pedido.choices = [(0, 'Selecione um tipo...')]
-
-
 
 
 class AlternativaForm(FlaskForm):
@@ -146,6 +128,7 @@ class AlternativaForm(FlaskForm):
     def __init__(self, *args, **kwargs):
         super(AlternativaForm, self).__init__(*args, **kwargs)
         self.circuito.choices = [(0, 'Selecione um circuito...')]
+
 
 class AnexoForm(FlaskForm):
     """Formulário para upload de anexos"""
