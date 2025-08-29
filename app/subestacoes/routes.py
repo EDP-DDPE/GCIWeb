1e10f3e283490907cd9ac0c67b2e66b6542f638b
GIOVANA DA PENHA SATEL <EX160187@edp.com.br>
2025-08-29 14:19:15 -0300

    minhas alterações locais

--- app/subestacoes/routes.py
+++ app/subestacoes/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request
-from app.models import Subestacao
+from app.models import Subestacao, Municipio, EDP
 
 subestacao_bp = Blueprint("subestacoes", __name__, template_folder="templates")
 
@@ -24,3 +24,27 @@ def editar_subestacao(id):
         return redirect(url_for("subestacoes.listar_subestacoes"))
 
     return render_template("editar_subestacao.html", sub=sub)
+
+@subestacao_bp.route("/subestacoes/nova", methods=["GET", "POST"])
+def nova_subestacao():
+    municipios = Municipio.query.all()
+    edps = EDP.query.all()
+
+    if request.method == "POST":
+        nome = request.form["nome"]
+        sigla = request.form["sigla"]
+        id_municipio = request.form["id_municipio"]
+        id_edp = request.form["id_edp"]
+
+        nova = Subestacao(
+            nome=nome,
+            sigla=sigla,
+            id_municipio=id_municipio,
+            id_edp=id_edp
+        )
+        db.session.add(nova)
+        db.session.commit()
+        flash("Subestação cadastrada com sucesso!", "success")
+        return redirect(url_for("subestacoes.listar_subestacoes"))
+
+    return render_template("nova_subestacao.html", municipios=municipios, edps=edps)
