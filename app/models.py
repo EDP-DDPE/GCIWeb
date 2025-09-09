53cd9ea2c261da4d226d4acb27807dc0eed3ee62
Jader Kayque Marques de Campos <jaderkayque@yahoo.com.br>
2025-09-09 17:20:17 -0300

    Adicionado model tabela instalacoes

--- app/models.py
+++ app/models.py
@@ -23,6 +23,25 @@ class EDP(db.Model):
     estudos = db.relationship('Estudo', back_populates='edp', lazy='select')
 
 
+class Instalacao(db.Model):
+    __tablename__ = 'INSTALACOES'
+    __table_args__ = {'schema': 'gciweb'}
+
+    ID_INSTALACAO = db.Column(db.BigInteger, primary_key=True)
+    EMPRESA = db.Column(db.Text, nullable=True)
+    INSTALACAO = db.Column(db.Text, nullable=True)
+    CNPJ = db.Column(db.Text, nullable=True)
+    CPF = db.Column(db.Text, nullable=True)
+    STATUS_INSTALACAO = db.Column(db.Text, nullable=True)
+    DESCRICAO_STATUS = db.Column(db.Text, nullable=True)
+    DESCRICAO_CLASSE = db.Column(db.Text, nullable=True)
+    TARIFA = db.Column(db.Text, nullable=True)
+    CARGA = db.Column(db.Numeric(10, 2), nullable=True)
+    TIPO_CLIENTE = db.Column(db.Text, nullable=True)
+    NOME_PARCEIRO = db.Column(db.Text, nullable=True)
+    CEP = db.Column(db.Text, nullable=True)
+
+
 class Usuario(db.Model):
     __tablename__ = 'usuarios'
     __table_args__ = {'schema': 'gciweb'}
